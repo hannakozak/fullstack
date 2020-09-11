d8d54f1e80c361f031668a835b2e50ea69a4ac7e
Hanna Kozak <hannawkozak@gmail.com>
2020-09-11 23:06:59 +0100

    1.2: course information, step2

--- part1/src/index.js
+++ part1/src/index.js
@@ -9,18 +9,18 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+	return (
+	<p>{props.part} {props.exercise}</p>
+	)
+}
+
 const Content = (props) => {
 	return (
     <div>
-       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+	  <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+	  <Part part={props.part3} exercise={props.exercises3} />
     </div>
   )
 }
