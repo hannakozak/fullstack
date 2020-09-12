d792aca95aa9eabbf8bd1dc8e3ee234cbbe90d46
Hanna Kozak <hannawkozak@gmail.com>
2020-09-12 23:13:25 +0100

    1.2: course information, step2

--- part1/course_information/src/index.js
+++ part1/course_information/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
 const Header = (props) => {
 	return (
     <div>
@@ -9,22 +8,21 @@ const Header = (props) => {
   )
 }
 
+const Part = (props) => {
+	return (
+	<p>{props.part} {props.exercise}</p>
+	)
+}
+
 const Content = (props) => {
 	return (
     <div>
-       <p>
-        {props.part1} {props.exercises1}
-      </p>
-      <p>
-        {props.part2} {props.exercises2}
-      </p>
-      <p>
-        {props.part3} {props.exercises3}
-      </p>
+	  <Part part={props.part1} exercise={props.exercises1} />
+      <Part part={props.part2} exercise={props.exercises2} />
+	  <Part part={props.part3} exercise={props.exercises3} />
     </div>
   )
 }
-
 const Total = (props) => {
 	return (
 	<p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
@@ -38,7 +36,6 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
-
   return (
     <div>
       <Header course={course} />
@@ -47,5 +44,4 @@ const App = () => {
     </div>
   )
 }
-
-ReactDOM.render(<App />, document.getElementById('root')) 
+ReactDOM.render(<App />, document.getElementById('root'))
