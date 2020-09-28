eeed836fd8c12d9c6bd52e503088cd113d111d59
Hanna Kozak <hannawkozak@gmail.com>
2020-09-28 22:52:16 +0100

    1.14*: anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,34 +1,53 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Mostvotes = ({ votes }) => {
+  const max = Math.max.apply(null, Object.values(votes))
+  const maxIndex = Object.keys(votes).find(key =>  votes[key] === max)
+   return (
+     <div>
+      { max>0 ?
+	    <div> 
+	      <h2>Anecdote with most votes </h2>
+		  <div>{anecdotes[maxIndex]}</div>
+		  <div>has { max } votes</div>
+		</div> :
+		<div>
+		  <h2>Anecdote with most votes </h2>
+	      <div>No votes yet</div>
+		</div> }
+	 </div>
+   )
+}
+
+const Button = ({ onClick, text }) => (
+  <button onClick={ onClick }>
+    { text }
+  </button>
+)
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
-
- const handleClick = () => {
-  setSelected(Math.floor(Math.random() * anecdotes.length))
+  
+  const handleClick = () => {
+    setSelected(Math.floor(Math.random() * anecdotes.length))
   }
-  console.log(selected)
   
   const handleVotes = () => {
-	  const newVotes = { ...votes }
-	  newVotes[selected]+=1
-	  setVotes(newVotes)
-  }
-  console.log(votes)
+    const newVotes = { ...votes }
+	newVotes[selected]+=1
+	setVotes(newVotes) 
+  }	
   
   return (
     <div>
-      <div>
-        {anecdotes[selected]}
-      </div>
-	  <div>
-	  has {votes[selected]} votes
-	  </div>
-	  <button onClick={handleVotes}>vote</button>
-	  <button onClick={handleClick}>
-	    next anecdote
-	  </button>
+	  <h2>Anectode of the day</h2>
+        <div>{ anecdotes[selected] }</div>
+	    <div>has { votes[selected] } votes</div>
+	   <Button onClick={handleVotes} text="vote"/>
+	   <Button onClick={handleClick} text="next anecdote"/>
+	   <Mostvotes votes={votes} />
     </div>
   )
 }
