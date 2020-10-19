597f214939edb2aa3aeb6ed49cfcb045c86a9fa6
Hanna Kozak <hannawkozak@gmail.com>
2020-10-19 23:17:00 +0100

    3.4: Phonebook backend step4

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -3,24 +3,24 @@ const app = express()
 
 let persons = [
     {
-      "name": "Arto Hellas",
-      "number": "211-222-333",
-      "id": 1
+      name: "Arto Hellas",
+      number: "211-222-333",
+      id: 1
     },
     {
-      "name": "Ada Lovelace",
-      "number": "123",
+      name: "Ada Lovelace",
+      number: "123",
       "id": 2
     },
     {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
+      name: "Dan Abramov",
+      number: "12-43-234345",
+      id: 3
     },
     {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
+      name: "Mary Poppendieck",
+      number: "39-23-6423122",
+      id: 4
     }
   ]
   
@@ -41,6 +41,13 @@ app.get('/api/persons/:id', (request, response) => {
   } else {
     response.status(404).end()
   }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  response.status(204).end()
 })
 
 const PORT = 3001
