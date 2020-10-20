0d1f3fdd6d41b57cbab455b4a9341adac28ac364
Hanna Kozak <hannawkozak@gmail.com>
2020-10-20 11:41:05 +0100

    3.5: Phonebook backend step5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     {
       name: "Arto Hellas",
@@ -50,6 +52,31 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+const generateId = () => {
+  const max = persons.length 
+   return Math.floor(Math.random() * Math.floor(max) + 1)
+}
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  
+
+  if (!body.name) {
+    return response.status(400).json({ 
+      error: 'content missing' 
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateId(),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
 const PORT = 3001
 app.listen(PORT)
 console.log(`Server running on port ${PORT}`)
