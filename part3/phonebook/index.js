9ef491ea726c0f815aa62fae7638dd71aa85ef43
Hanna Kozak <hannawkozak@gmail.com>
2020-10-20 13:11:40 +0100

    3.7: Phonebook backend step7

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,25 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+
+app.use(
+  morgan(
+  ':method :url :status :res[content-length] - :response-time ms'
+  )
+);
+
+morgan(function (tokens, req, res) {
+  return [
+    tokens.method(req, res),
+    tokens.url(req, res),
+    tokens.status(req, res),
+    tokens.res(req, res, 'content-length'), '-',
+    tokens['response-time'](req, res), 'ms'
+  ].join(' ')
+})
 
 let persons = [
     {
