8d8767afff0c65b79e13a1af8a58dbd7d68d43d2
Hanna Kozak <hannawkozak@gmail.com>
2020-10-20 11:54:00 +0100

    3.6: Phonebook backend step6

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -63,7 +63,19 @@ app.post('/api/persons', (request, response) => {
 
   if (!body.name) {
     return response.status(400).json({ 
-      error: 'content missing' 
+      error: 'name missing' 
+    })
+  }
+  
+  if (!body.number) {
+    return response.status(400).json({ 
+      error: 'number missing' 
+    })
+  }
+  
+  if (persons.find(person => person.name === body.name)){
+	return response.status(400).json({ 
+      error: 'name must be unique' 
     })
   }
 
