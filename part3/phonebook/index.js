897890fff2bfb4117885af22cc9552db3165f1b3
Hanna Kozak <hannawkozak@gmail.com>
2020-10-21 22:36:50 +0100

    3.10 phonebook backend step10

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -103,6 +103,7 @@ app.post('/api/persons', (request, response) => {
 
   response.json(person)
 })
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT
+app.listen(PORT), () => {
+  console.log(`Server running on port ${PORT}`)
+}
