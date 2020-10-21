fbcad7076a411b5978c99cda07fc7b86279a5fed
Hanna Kozak <hannawkozak@gmail.com>
2020-10-21 22:54:07 +0100

    phonebook backend

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = 'https://murmuring-beach-03166.herokuapp.com/api/persons'
+const baseUrl = 'api/persons'
 
 const getAll = () => {
   const request = axios.get(baseUrl)
