b528e29cb9bb918c7af41b42c10efe465dae2c7d
Hanna Kozak <hannawkozak@gmail.com>
2020-11-10 11:56:30 +0000

    4.15: bloglist expansion, step3

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -2,8 +2,12 @@ const supertest = require('supertest')
 const mongoose = require('mongoose')
 const app = require('../app')
 const helper = require('./test_helper')
-const Blog = require('../models/blog')
 const api = supertest(app)
+const bcrypt = require('bcrypt')
+
+const Blog = require('../models/blog')
+const User = require('../models/user')
+
 
 beforeEach(async () => {
   await Blog.deleteMany({})
@@ -117,7 +121,61 @@ describe('addition of a new blog post', () => {
       const title = blogsAtEnd.map(r => r.title)
       expect(title).not.toContain(blogToDelete.title)
     })
+ })
+ 
+ describe('when there is initially one user in db', () => {
+  beforeEach(async () => {
+    await User.deleteMany({})
+
+    const passwordHash = await bcrypt.hash('sekret', 10)
+    const user = new User({ username: 'root', passwordHash })
+
+    await user.save()
   })
+
+  test('creation succeeds with a fresh username', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'mluukkai',
+      name: 'Matti Luukkainen',
+      password: 'salainen',
+    }
+
+    await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length + 1)
+
+    const usernames = usersAtEnd.map(u => u.username)
+    expect(usernames).toContain(newUser.username)
+  })
+  
+  test('creation fails with proper statuscode and message if username already taken', async () => {
+    const usersAtStart = await helper.usersInDb()
+
+    const newUser = {
+      username: 'root',
+      name: 'Superuser',
+      password: 'salainen',
+    }
+
+    const result = await api
+      .post('/api/users')
+      .send(newUser)
+      .expect(400)
+      .expect('Content-Type', /application\/json/)
+
+    expect(result.body.error).toContain('`username` to be unique')
+
+    const usersAtEnd = await helper.usersInDb()
+    expect(usersAtEnd).toHaveLength(usersAtStart.length)
+  })
+})
 
 afterAll(() => {
   mongoose.connection.close()
