04f3c01b4bf63bdeb8a167785a5d721d64d03849
Hanna Kozak <hannawkozak@gmail.com>
2020-12-07 22:33:27 +0000

    5.18: bloglist end to end testing, step2

--- part5/bloglist_frontend/cypress/integration/blog_app.spec.js
+++ part5/bloglist_frontend/cypress/integration/blog_app.spec.js
@@ -1,19 +1,42 @@
 describe('Blog app', function() {
   beforeEach(function() {
-
     cy.request({
       method: 'POST',
       url: 'http://localhost:3001/api/testing/reset',
       failOnStatusCode: false
-  })
-    
+    })
+    const user = {
+      name: 'Anna Noga',
+      username: 'anna',
+      password: 'annanoga'
+    }
+    cy.request({
+      method: 'POST',
+      url: 'http://localhost:3001/api/users/',
+      failOnStatusCode: false
+    })
     cy.visit('http://localhost:3000')
   })
 
-  it('Login form is shown', function() {
-    cy.contains('Login to application')
-  })
+    it('Login form is shown', function() {
+      cy.contains('Login to application')
+    })
 
-  
+    describe('Login',function() {
+      it('succeeds with correct credentials', function() {
+        cy.contains('login').click()
+        cy.get('#username').type('anna')
+        cy.get('#password').type('annanoga')
+        cy.get('#login-button').click()
+        cy.contains('Anna Noga is logged in')
+      })
+
+      it('fails with wrong credentials', function() {
+        cy.contains('login').click()
+        cy.get('#username').type('wrong')
+        cy.get('#password').type('wrong')
+        cy.get('#login-button').click()
+      })
+    })
 })
 
--- part5/bloglist_frontend/src/components/LoginForm.js
+++ part5/bloglist_frontend/src/components/LoginForm.js
@@ -14,13 +14,13 @@ const LoginForm = ({
             <form onSubmit={handleSubmit}>
                 <div>
           username
-                    <input value={username} onChange={handleUsernameChange} />
+                    <input id="username" value={username} onChange={handleUsernameChange} />
                 </div>
                 <div>
           password
-                    <input type="password" value={password} onChange={handlePasswordChange} />
+                    <input id="password" type="password" value={password} onChange={handlePasswordChange} />
                 </div>
-                <button type="submit">login</button>
+                <button id="login-button" type="submit">login</button>
             </form>
         </div>
     )
